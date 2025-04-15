d8cff239161e4d1ec09bd2d337bb26d588b6b2ca
PhCorrea-hash <pedrohenriquemartinscorrea@gmail.com>
2025-04-15 17:33:47 -0300

    PostgreSQL

--- setup/settings.py
+++ setup/settings.py
@@ -86,12 +86,18 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),  # precisa existir
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
+print("DB key:", os.getenv('DATABASE_URL'))
 
 
 # Password validation
@@ -172,3 +178,6 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+#db
+
